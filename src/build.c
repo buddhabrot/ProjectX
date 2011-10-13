50a791730b796dddeccb1271b841209d9aa5e388
Maarten Mortier <maarten.mortier@gmail.com>
2011-10-13 22:29:44 +0200

    Example XCode project layout.

---
+++ src/build.c
@@ -0,0 +1,14 @@
+/*
+ *  build.c
+ *  ProjectX
+ *
+ *  Created by Liesbet on 13/10/11.
+ *  Copyright 2011 __MyCompanyName__. All rights reserved.
+ *
+ */
+
+#include "build.h"
+
+int main(int argc, char * argv[]) {
+	printf("The build works.");
+}
---
+++ src/build.h
@@ -0,0 +1,9 @@
+/*
+ *  build.h
+ *  ProjectX
+ *
+ *  Created by Liesbet on 13/10/11.
+ *  Copyright 2011 __MyCompanyName__. All rights reserved.
+ *
+ */
+
